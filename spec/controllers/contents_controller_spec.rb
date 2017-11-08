c8a0fd798136294984c69c89b815925123ddb45a
Saraswati Saud <saraswati.saud@gmail.com>
2017-11-08 19:40:09 +0545

    Create Controller test

--- spec/controllers/contents_controller_spec.rb
+++ spec/controllers/contents_controller_spec.rb
@@ -2,18 +2,27 @@ require 'rails_helper'
 
 RSpec.describe ContentsController, type: :controller do
 
-  describe "GET #index" do
-    it "returns http success" do
+  describe 'GET #index' do
+    it 'responds successfully' do
       get :index
-      expect(response).to have_http_status(:success)
+      expect(response).to be_success
+    end
+
+    it 'returns a 200 response' do
+      get :index
+      expect(response).to have_http_status '200'
     end
   end
 
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+  describe 'POST #create' do
+    it 'responds successfully' do
+      post :create
+      expect(response).to be_success
+    end
+
+    it 'returns a 200 response' do
+      post :create
+      expect(response).to have_http_status '200'
     end
   end
-
 end
